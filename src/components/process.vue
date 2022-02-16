6680bf648d7a0b5aabf624b5e6323c452b0913e0
Fiyinfoluwa John Ajala <johnajala204@gmail.com>
2022-02-16 22:55:04 +0100

    hot ui fix

--- src/components/process.vue
+++ src/components/process.vue
@@ -4,7 +4,7 @@
 
         <div class="flex justify-between space-x-14">
             <section class="w-[33em] relative grid grid-cols-3 gap-3 rounded-xl hover:shadow-sm transition-all duration-300 bg-pink opacity-95">
-                <span class="absolute text-xl font-bold -top-7 left-3">1</span>
+                <span class="absolute decoration-pink-dark decoration-solid overline decoration-4 text-xl font-bold -top-7 left-3">1</span>
                 <div class="py-8 px-6 col-span-2">
                     <h2 class="font-poppins text-[1.3em] font-bold leading-6 mb-3 text-red">Schedule an appointment with an expert psychiatrist</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat rerum asperiores libero.</p>
@@ -15,7 +15,7 @@
             </section>
 
             <section class="w-[33em] relative grid grid-cols-3 gap-3 rounded-xl hover:shadow-sm transition-all duration-300 bg-pink opacity-95">
-                <span class="absolute text-xl font-bold -top-7 left-3">2</span>
+                <span class="absolute decoration-pink-dark decoration-solid overline decoration-4 text-xl font-bold -top-7 left-3">2</span>
                 <div class="py-8 px-6 col-span-2">
                     <h2 class="font-poppins text-[1.3em] font-bold leading-6 mb-3 text-red">Schedule an appointment with an expert psychiatrist</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat rerum asperiores libero.</p>
@@ -26,7 +26,7 @@
             </section>
 
             <section class="w-[33em] relative grid grid-cols-3 gap-3 rounded-xl hover:shadow-sm transition-all duration-300 bg-pink opacity-95">
-                <span class="absolute text-xl font-bold -top-7 left-3">3</span>
+                <span class="absolute decoration-pink-dark decoration-solid overline decoration-4 text-xl font-bold -top-7 left-3">3</span>
                 <div class="py-8 px-6 col-span-2">
                     <h2 class="font-poppins text-[1.3em] font-bold leading-6 mb-3 text-red">Schedule an appointment with an expert psychiatrist</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat rerum asperiores libero.</p>
